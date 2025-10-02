8b823925e3001562dc304184d965062cfa56a8b8
Patryk333 <kowalikpatryk02@gmail.com>
2025-10-02 10:15:33 +0200

    prueba

--- src/components/content.js
+++ src/components/content.js
@@ -1,16 +1,5 @@
 export {renderContent}
 
 function renderContent(){
-  const fruitsBoard = Array(120).fill(0).map((_,i)=>i)
-    return `
-<div class="container board-wrapper">
-  <div class="board">
-  ${
-    fruitsBoard.map(f=>`<div class="cell">${f}</div>`).join('')
-  }
-  
-   
-  </div>
-</div>
-    `;
+
 }
