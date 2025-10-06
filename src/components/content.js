03092b5777b0517904fde2fa5604b2f0db54dc1b
Patryk333 <kowalikpatryk02@gmail.com>
2025-10-06 13:42:27 +0200

    Tablero inicial

--- src/components/content.js
+++ src/components/content.js
@@ -1,31 +1,23 @@
-export { renderContent }
+export { renderContent };
+
+const tamaño = 15;
 
 function renderContent() {
-  const filas = 12;
-  const columnas = 10;
-  const anchoCelda = 40;
-  const altoCelda = 40;
+  let html = `
+    <div class="board-wrapper">
+      <div class="board">
+  `;
 
-  const html = `
-    <div class="container board-wrapper">
-      <canvas id="boardCanvas" width="${columnas * anchoCelda}" height="${filas * altoCelda}"></canvas>
+  for (let y = 0; y < tamaño; y++) {
+    for (let x = 0; x < tamaño; x++) {
+      html += `<div class="cell empty"><div class="inner"></div></div>`;
+    }
+  }
+
+  html += `
+      </div>
     </div>
   `;
 
-  const app = document.getElementById('app');
-  app.innerHTML = html;
-
-  const canvas = document.getElementById('boardCanvas');
-  const ctx = canvas.getContext('2d');
-  const colorCelda = 'blue';
-
-  for (let i = 0; i < filas; i++) {
-    for (let j = 0; j < columnas; j++) {
-      ctx.fillStyle = colorCelda;
-      ctx.fillRect(j * anchoCelda, i * altoCelda, anchoCelda, altoCelda);
-
-      ctx.strokeStyle = 'black';
-      ctx.strokeRect(j * anchoCelda, i * altoCelda, anchoCelda, altoCelda);
-    }
-  }
+  return html;
 }
